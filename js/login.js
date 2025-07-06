0ae275f469ce0702619b95dca1bc32b054e47f8c
Tyler Morency <Tylermlucis@gmail.com>
2025-07-05 22:00:17 -0400

    added new files as well as fixing file structure added base level password authentication and the ablity to reset password will be working on pages and linking database next

---
+++ js/login.js
@@ -0,0 +1,28 @@
+function loginvalidation() {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const loginError = document.getElementById("login-error");
+  
+
+    loginError.textContent = "";
+    loginError.style.display = "none";
+  
+    
+    if (username !== "t" || password !== "l") {
+      loginError.textContent = "Invalid credentials";
+      loginError.style.color = "red";
+      loginError.style.display = "block";
+      return false;
+    }
+  
+    loginError.textContent = "Login successful!";
+    loginError.style.color = "green";
+    loginError.style.display = "block";
+  
+  
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1000);
+  
+    return false; 
+  }
